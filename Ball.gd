0ccf512324e1919adb37587bbe4d64eba11ee1de
Souvik Kar Mahapatra <souvikat001@gmail.com>
2024-03-24 16:02:25 +0530

    add: ball consumption

--- Ball.gd
+++ Ball.gd
@@ -2,17 +2,50 @@ extends RigidBody2D
 
 class_name Ball
 
-var ball_state = null
+var id = 0
 
-# Called when the node enters the scene tree for the first time.
+var is_initial_position_used = false
+var is_playing = false
+var state = null
+var collided = false
+
+var consuming_data = []
+
+func _integrate_forces(s:Physics2DDirectBodyState):
+	self.rotation_degrees = 0
+	if not is_playing:
+		if state:
+			s.transform.origin = Vector2(state.pos[0], state.pos[1])
+			#s.linear_velocity = Vector2(state.vel[0], state.vel[1])
+		print("Consumed")
+	if s.get_contact_count() > 0:
+		var collision_pos = to_local(s.get_contact_local_position(0))
+		print(">>>> ", collision_pos, s.get_contact_collider_position(0))
+		print({"collider_type":s.get_contact_collider(0), "collider_pos":s.get_contact_collider_position(0), "collider_vel":s.get_contact_collider_velocity_at_position(0)})
+	"""
+	if sleeping:
+		s.linear_velocity = Vector2.ZERO
+	
+	if not is_initial_position_used and not is_playing and state:
+		print("consuming state...")
+		s.transform.origin = Vector2(state.pos[0], state.pos[1])
+		s.linear_velocity = Vector2(state.vel[0], state.vel[1])
+		is_initial_position_used = true
+	"""
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _physics_process(delta):
-	ball_state = {"v": linear_velocity, "p":global_position, "c": false }
+	if is_playing:
+		id+=1
+		state = {"id": id, "vel": [linear_velocity.x,linear_velocity.y], "pos":[global_position.x,global_position.y], "collided": collided }
+	else:
+		#print(">>>>>>> ",{"pos":[global_position.x, global_position.y],"cpos":state.pos})
+		consuming_data.push_back({"id": state.id, "vel": [linear_velocity.x,linear_velocity.y], "pos":[global_position.x,global_position.y], "collided": collided})
+	collided = false
 	$Label.text = "{v}\n{p}".format({"v": linear_velocity, "p":global_position })
 
 
 func _on_Area2D_body_entered(body):
-	ball_state = {"v": linear_velocity, "p":global_position, "c": body.is_in_group("Player") }
+	collided = true
--- Board.gd
+++ Board.gd
@@ -1,30 +1,64 @@
 extends Node2D
 
+var id = 0
+
+var is_playing = false
 var board_state = null
+var consume_data = []
+var stream_data = []
 
-# Called when the node enters the scene tree for the first time.
+var file = null
+var stream_file_path = "res://stream_data.json"
+var consume_file_path = "res://consume_data.json"
+
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-"""
-func _physics_process(delta):
-	board_state = $Player.player_state
-	print("Board.gd {x}".format({"x":board_state}))
-"""
+	$Player.is_playing = is_playing
+	$Ball.is_playing = is_playing
+	file = File.new()
+	if not is_playing:
+		load_file_data(stream_file_path)
 
 func _physics_process(delta):
-	pass
-	#var ball_position = $Ball.global_position
-	#$Player.look_at(ball_position)
+	if not is_playing:
+		board_state = consume_data.pop_front()
+		if board_state:
+			$Ball.state = board_state.ball
+			$Player.state = board_state.paddle
+	else:
+		if board_state:
+			stream_data.push_back(board_state)
+		if $Player.state and $Ball.state:
+			id+=1
+			board_state = {
+				"id": id,
+				"ball": $Ball.state,
+				"paddle": $Player.state
+			}
 
 func get_state():
-	return $Player.player_state
-	#print("Board.gd {x}".format({"x":board_state}))
+	return { "ball": $Player.state, "paddle": $Ball.state }
 
 func set_state(d):
-	$Player.player_state = d
+	board_state = d
 
 func _exit_tree():
+	if is_playing:
+		print("WRITE TO FILE")
+		save_file_data(stream_file_path,stream_data)
+	else:
+		save_file_data(consume_file_path, {"ball":$Player.consuming_data, "paddle":$Player.consuming_data})
 	print("EXIT from Board")
 
+func load_file_data(file_name):
+	file.open(file_name, File.READ)
+	var data = file.get_as_text()
+	consume_data = parse_json(data)
+	file.close()
+	print("File loaded!")
+
+func save_file_data(file_name, data):
+	file.open(file_name, File.WRITE)
+	file.store_line(to_json(data))
+	stream_data = []
+	file.close()
+	print("File saved")
